4c4e1d6309feb053bad7f2404828e1e71b43eeaa
Polkrit Bualoy <pkblnthn@gmail.com>
2026-02-02 22:06:25 +0700

    update Title Screen Structure

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
+class TitleScreen(Screen):
+    pass
+
+
 class BattleScreen(Screen):
     pass
 
@@ -11,7 +15,10 @@ class BattleScreen(Screen):
 class TurnBasedApp(App):
     def build(self):
         Window.size = (720, 480)
-        return BattleScreen()
+        sm = ScreenManager()
+        sm.add_widget(TitleScreen(name="title"))
+        sm.add_widget(BattleScreen(name="battle"))
+        return sm
 
 
 if __name__ == "__main__":
