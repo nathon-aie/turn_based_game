bf072b2190140d9519dffd4cd7aa90615ee9fd09
Polkrit Bualoy <pkblnthn@gmail.com>
2026-02-11 21:47:31 +0700

    add world screen

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.popup import Popup
 from kivy.factory import Factory
+from kivy.properties import StringProperty
+from kivy.event import EventDispatcher
 
 
 class TitleScreen(Screen):
     def play_button(self):
         self.manager.transition.direction = "left"
         self.manager.current = "battle"
+
+
+class WorldScreen(Screen):
+    pass
 
 
 class BackpackPopup(Popup):
@@ -58,10 +64,11 @@ class TurnBasedApp(App):
 
     def build(self):
         Window.size = (720, 480)
-        sm = ScreenManager()
-        sm.add_widget(TitleScreen(name="title"))
-        sm.add_widget(BattleScreen(name="battle"))
-        return sm
+        # sm = ScreenManager()
+        # sm.add_widget(TitleScreen(name="title"))
+        # sm.add_widget(BattleScreen(name="battle"))
+        # return sm
+        return WorldScreen()
 
 
 if __name__ == "__main__":
