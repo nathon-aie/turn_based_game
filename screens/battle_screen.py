999fb09502cd2ef0a9051d0e829ee169f547201c
Polkrit Bualoy <pkblnthn@gmail.com>
2026-02-14 01:35:55 +0700

    update battle screen by adding enemy and hero widgets

--- screens/battle_screen.py
+++ screens/battle_screen.py
@@ -1,11 +1,54 @@
 from kivy.uix.screenmanager import Screen
 from widgets.backpack import BackpackPopup
 from kivy.lang import Builder
+import random
 
 Builder.load_file("screens/battle_screen.kv")
 
 
+class Enemy:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.max_hp = hp
+
+
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.max_hp = hp
+
+
 class BattleScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.current_enemy = None
+
+    def on_enter(self):
+        self.spawn_hero()
+        self.spawn_enemy()
+        self.update_ui()
+
+    def spawn_hero(self):
+        self.hero = Hero("Mambo", 100)
+
+    def spawn_enemy(self):
+        possible_enemies = [
+            Enemy("Noob Slayer", 50),
+            Enemy("Goblin Slayer", 80),
+            Enemy("God Slayer", 30),
+        ]
+        self.current_enemy = random.choice(possible_enemies)
+
+    def update_ui(self):
+        if self.current_enemy:
+            display_text = f"{self.current_enemy.name}\nHP: {self.current_enemy.hp}"
+            self.ids.enemy_stat_label.text = display_text
+        if self.hero:
+            display_text = f"{self.hero.name}\nHP: {self.hero.hp}"
+            self.ids.hero_stat_label.text = display_text
+
     def skill_1_button(self):
         pass
 
@@ -28,4 +71,4 @@ class BattleScreen(Screen):
 
     def run_button(self):
         self.manager.transition.direction = "right"
-        self.manager.current = "title"
+        self.manager.current = "world"
